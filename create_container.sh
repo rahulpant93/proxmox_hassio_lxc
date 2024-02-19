f40cffccfd7355aeac2085ae7a1a467c7c1018fa
Тютюнников Максим <max-tyutyunnikov@yandex.ru>
2024-02-19 21:09:42 +0300

    Auto

--- create_container.sh
+++ create_container.sh
@@ -140,12 +140,12 @@ lxc-cmd wget -qLO $NETWORKMANAGER_CONFIG_PATH ${HA_URL_BASE}/etc/NetworkManager/
 msg "Configuring NetworkManager... 2"
 lxc-cmd sed -i 's/type\:veth/interface-name\:veth\*/' $NETWORKMANAGER_CONFIG_PATH
 msg "Configuring NetworkManager... 3"
-#NETWORKMANAGER_PROFILE_PATH='/etc/NetworkManager/system-connections/default'
-#msg "Configuring NetworkManager... 4"
-#lxc-cmd wget -qLO $NETWORKMANAGER_PROFILE_PATH ${HA_URL_BASE}/system-connection-default
-#msg "Configuring NetworkManager... 5"
-#lxc-cmd chmod 600 $NETWORKMANAGER_PROFILE_PATH
-#msg "Configuring NetworkManager... 6"
+NETWORKMANAGER_PROFILE_PATH='/etc/NetworkManager/system-connections/default'
+msg "Configuring NetworkManager... 4"
+lxc-cmd wget -qLO $NETWORKMANAGER_PROFILE_PATH https://github.com/MaxTyutyunnikov/proxmox_hassio_lxc/raw/master/system-connection-default
+msg "Configuring NetworkManager... 5"
+lxc-cmd chmod 600 $NETWORKMANAGER_PROFILE_PATH
+msg "Configuring NetworkManager... 6"
 NETWORKMANAGER_CONNECTION=$(lxc-cmd nmcli connection | grep eth0 | awk -F "  " '{print $1}')
 msg "Configuring NetworkManager... 7"
 lxc-cmd nmcli connection down "$NETWORKMANAGER_CONNECTION" > /dev/null
